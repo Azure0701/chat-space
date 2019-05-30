f148438bf4699cb0bbea0e71857851e4251b0921
Azure0701 <s.tzw701@gmail.com>
2019-05-30 17:51:08 +0900

    メッセージの非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,30 @@
 $(function() {
+  
+  function buildHTML(message){
+    var addImage = (message.image !== null) ? `<img class = "lower-message__image", src="${message.image}"> ` : ''
+
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                      ${addImage}
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -11,6 +32,15 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop:$('.message')[0].scrollHeight});
+      $('.form__message').val('');
+    })
+      .fail(function(){
+        alert('error');
     })
   })
 });
