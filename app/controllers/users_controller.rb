fe79f41d7d34808d592d7e766616ea808e5b4a12
Azure0701 <s.tzw701@gmail.com>
2019-06-01 15:40:52 +0900

    インクリメンタルサーチの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.where('name LIkE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
+    @user =User.find(params[:id])
   end
 
   def update
