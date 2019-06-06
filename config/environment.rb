887b649727bea90f54aa7dab7ca31312a797a5f8
Azure0701 <s.tzw701@gmail.com>
2019-06-07 01:54:50 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+DATABASE_PASSWORD='azureflamegod0701'
+SECRET_KEY_BASE='683037b30ed37a65c8687c92d3c79b447ce20d02fa0683453b290b37e0932d7240d2f7f26486967c3f42d82fc9799c32ab7f359fdf905ea1acf40b976ababb7d'
