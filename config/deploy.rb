211c172642af2a6ce0bc96fc58e96ed1efd9e9db
Azure0701 <s.tzw701@gmail.com>
2019-06-07 13:51:14 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['Users/tazawashota/.ssh/azureflamegod0701.pem']
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
